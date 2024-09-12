2b4131ce379405ec439ca6132b35c8ecdbff57a9
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-12 04:17:52 -0400

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -225,6 +225,12 @@ router.post('/setOnOff', async function (req, res) {
     })
 });
 
-
+router.post('/forgot', async function (req, res) {
+    var data = await model.forgot(req.body)
+    return res.json({
+        code: 20000,
+        data: data
+    })
+});
 
 module.exports = router;
