3c650d6eca1bdcdba5c74a0b7967027fd72bfb2c
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-12 04:16:39 -0400

    Update app.js

--- routes/app.js
+++ routes/app.js
@@ -15,7 +15,13 @@ const Storage = multer.diskStorage({
   })
 
 const upload = multer({ dest: '/' })
-
+router.post('/client_signup', async function (req, res) {
+    var data = await model.client_signup(req.body)
+    return res.json({
+        code: 20000,
+        data: data
+    })
+});
 
 router.post('/login', async function (req, res) {
     var data = await model.login(req.body)
