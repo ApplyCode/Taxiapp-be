7641006a335fc922ecde37ddd2176f64ea87d36c
SmileAchiever <128380874+ApplyCode@users.noreply.github.com>
2024-09-11 22:40:57 -0400

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,9 +1,4 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const fileUpload = require('./lib/index');
+
 
 var appRouter = require('./routes/app');
 var app = express();
